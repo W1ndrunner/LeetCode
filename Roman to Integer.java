b39c944c3a5c42f7420e11ee1cdb305214532e13
W1ndrunner <lewislanek3264@gmail.com>
2022-12-16 02:13:50 +0000

    Decided Method

--- Roman to Integer.java
+++ Roman to Integer.java
@@ -1,6 +1,27 @@
+import java.io.*;
+import java.util.*;
 class solution {
     public int romanToInt(String s) {
         int answer = 0;
+        char current;
+        char nextone;
+        int currentvalue;
+        int nextvalue;
+        boolean anomaly = false;
+        char[] numerals = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+        int[] values = {1, 5, 10, 50, 100, 500, 1000};
+        char[] innumerals = s.toCharArray();
+        for (int i=0; i < s.length(); i++) {
+            if (anomaly == true) {
+                continue;
+            } else {
+                 
+            }
+            
+        }
+
+
+
         return answer;
     }
 }
