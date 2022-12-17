72a48f80aec6e389459f21fe6612206f482d38ed
W1ndrunner <lewislanek3264@gmail.com>
2022-12-17 02:19:17 +0000

    Started. Figuring out method.

---
+++ LeetCode 75 - Level 1/Find Pivot Index.java
@@ -0,0 +1,15 @@
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+        boolean exists = false;
+        int leftsum;
+        int rightsum;
+        for (int i=0; i < nums.length; i++) {
+            
+                
+            
+
+
+        }
+    }
+}
