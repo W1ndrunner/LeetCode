5c9eed4c1b3f21c91cf07b7efd442c2eddebf91d
W1ndrunner <lewislanek3264@gmail.com>
2022-12-17 18:18:12 +0000

    First attempt

--- LeetCode 75 - Level 1/Find Pivot Index.java
+++ LeetCode 75 - Level 1/Find Pivot Index.java
@@ -2,14 +2,33 @@
 class Solution {
     public int pivotIndex(int[] nums) {
         boolean exists = false;
-        int leftsum;
-        int rightsum;
+        int[] leftsum = new int[nums.length];
+        int[] rightsum = new int[nums.length];
+        int index = -1;
+        int sum1 = 0;
+        int sum2 = 0;
         for (int i=0; i < nums.length; i++) {
-            
-                
-            
-
-
+            if (i == 0) {
+                leftsum[0] = 0;
+                rightsum[(nums.length - 1)] = 0;
+            } else {
+                sum1 += nums[i];
+                leftsum[i] = sum1;
+                sum2 += nums[(nums.length-i)];
+                rightsum[(nums.length - i - 1)] = sum2;
+            }
+        
         }
+        for (int j = 0; j < nums.length; j++) {
+            if (leftsum[j] == rightsum[j]){
+                index = j;
+                exists = true;
+                break;
+            }
+        }
+        if (exists = false) {
+            index = -1;
+        }
+        return index;
     }
 }
