7385b2ab692fec0dd9898efd9c267b283aedeada
W1ndrunner <lewislanek3264@gmail.com>
2022-12-27 20:07:12 +0000

    Started

---
+++ LeetCode 75 - Level 1/Isomorphic Strings.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int j = 0;
+        for (int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            Map<Integer, String> sDic = new HashMap<Integer, String>();
+            if (sDic.containsValue(c) == true) {
+                continue;
+            } else {
+                sDic.put(j, c);
+
+
+            }
+
+
+
+            
+        }
+
+        return true;
+    }
+}
