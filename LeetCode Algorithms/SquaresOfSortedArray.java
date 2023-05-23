2a26e459f333b3862cf7fb368649a9df64ff580f
W1ndrunner <lewislanek3264@gmail.com>
2023-05-23 21:27:17 +0100

    Caved.

--- LeetCode Algorithms/SquaresOfSortedArray.java
+++ LeetCode Algorithms/SquaresOfSortedArray.java
@@ -8,7 +8,7 @@ public class SquaresOfSortedArray {
         int i = 0;
         int j = n-1;
         for (int p = n-1; p >= 0; p --){
-            if (Math.abs(nums[i]) > Maths.abs(nums[j])){
+            if (Math.abs(nums[i]) > Math.abs(nums[j])){
                 result[p] = nums[i] * nums[i];
                 i++;
             } else{
