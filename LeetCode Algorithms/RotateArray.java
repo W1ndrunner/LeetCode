14c4488ba4c6774e6671fe72c5fffe6059b5f085
W1ndrunner <lewislanek3264@gmail.com>
2023-06-14 18:25:15 +0100

    Trying new method

--- LeetCode Algorithms/RotateArray.java
+++ LeetCode Algorithms/RotateArray.java
@@ -1,21 +1,20 @@
 public class RotateArray {
     public static void main(String[] args){
         int[] nums = {1,2,3,4,5,6};
-        System.out.println("We got: " + rotate(nums, 3));
+        System.out.println("We got: " + rotator(nums));
+        
     }
     public static int[] rotate(int[] nums, int k){
-            int n = nums.length;
-            int[] result = new int[n];
-            int[] temp = nums;
-            for (int i = k; i > 0; i--){
-                result[0] = temp[n-1];
-                for (int j = 1; j < n; j++){
-                    result[j] = temp[j-1];
-                }
-                temp = result;
-
-            }
-            return result;
+       return new int[0];
+    }
+    public static int[] rotator(int[] num){
+        int[] newnum = new int[num.length];
+        int temp = num[num.length-1];
+        for (int i = 0; i < num.length -2; i++){
+            newnum[i+1] = num[i];
+        }
+        newnum[0] = temp;
+        return newnum;
     }
     
 }
